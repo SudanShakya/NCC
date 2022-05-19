4cee606546a2474623d82ae14fb1427776271673
hello <sudanshakya15@gmail.com>
2022-05-19 14:25:37 +0545

    DataTypes commited

---
+++ NCC.ConsoleApp/NCC.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace NCC.ConsoleApp
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            DataTypes();
+        }
+
+        private static void DataTypes()
+        {
+            int i = 10;
+            i = 20;
+
+            //char => int => long => float => double
+            char c = 'C';
+            i = c;
+            long l = i;
+            float f = l;
+            double d = f;
+
+            f = (float)d;
+            l = (long)f;
+            i = (int)l;
+            c = (char)i;
+
+            var x = 1;
+            x = 10;
+
+            object o = "";
+            o = 1;
+            o = 2f;
+
+            var y = (float)o;
+
+            var a = "1";
+            var b = 2;
+
+            object o1 = "";
+            object o2 = 1;
+
+            var str = i.ToString();
+        }
+    }
+}
